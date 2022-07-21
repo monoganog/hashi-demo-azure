02d770435e42a92e001999ec9ea8f26b2688af9d
dan-peacock <dan@dan-rr4mktk9yh.home>
2022-07-21 16:18:07 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,70 @@
+#Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
+}
+
+#Create the virtual network
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+#Create internal subnet
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+#Specify VM options
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = var.vm_size
+  #Delete the disks automatically when deleting the VM
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = var.default_pass
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+      environment = var.tags
+      
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "prefix" {
+  default = "dan-peacock"
+}
+
+variable "vm_size" {
+    description = "Value of the vm_size for the azure VM"
+    type = string
+    default = "Standard_DS1_v2"
+}
+
+variable "default_pass" {
+    description = "inital password for VM"
+    type = string
+    default = "Password1234!"
+}
+
+variable "tags" {
+  default     = ""
+  description = "Additional resource tags"
+  type        = string
+}
