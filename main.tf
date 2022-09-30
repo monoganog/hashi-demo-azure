a8548f7344b96b057a159cfe7597b790bc488fa9
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2022-09-30 11:48:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,3 +78,11 @@ resource "azurerm_virtual_machine" "main" {
       
   }
 }
+
+resource "azurerm_management_lock" "resource-group-level" {
+  depends_on = [azurerm_virtual_machine.main]
+  name       = "resource-group-level"
+  scope      = azurerm_resource_group.example.id
+  lock_level = "ReadOnly"
+  notes      = "This Resource Group is Read-Only"
+}
