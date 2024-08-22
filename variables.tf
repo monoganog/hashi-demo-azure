7fa4aa03b0345ec52eac533edc59d3cd64e6cbe8
Ben Monaghan <monoganog@gmail.com>
2024-08-22 23:57:50 +0100

    change vm type

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "prefix" {
 variable "vm_size" {
     description = "Value of the vm_size for the azure VM"
     type = string
-    default = "Standard_A1"
+    default = "Standard_DS1_v2"
 }
 
 variable "default_password" {
